3f93bbabbb1538196a7191c0ed6ffda077d68006
samim.azimi <samimelite8@gmail.com>
2022-01-18 16:00:20 +0430

    Comming Soon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './style/root.css'
 import React from 'react'
-import Nav from './components/Navigation.js'
-import Header from './components/Header.js'
-import Footer from './components/Footer.js'
+// import Nav from './components/Navigation.js'
+// import Header from './components/Header.js'
+// import Footer from './components/Footer.js'
 import {
   Routes,
   Route
@@ -12,12 +12,11 @@ import ContactUs from './components/contactus.js'
 import AboutUs from './components/AboutUs.js'
 import Services from './components/Services.js'
 import CustomerPortal from './components/CustomerPortal.js'
-
+import commingsoon from './assets/commingsoon.jpeg'
 function App() {
   return (
     <>
-      <Header />
-      <Nav />
+      <img src={commingsoon} alt="CommingSoon" />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="contactus" element={<ContactUs />} />
@@ -25,7 +24,7 @@ function App() {
         <Route path="services" element={<Services />} />
         <Route path="customerportal" element={<CustomerPortal />} />
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
     return (
         <div>
-            <h1>Home</h1>
+            <h1 className="commingSoon">Tune Up for Our New Online Services</h1>
         </div >
     )
 }
