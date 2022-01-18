f1cc8de9a739af675a3ba9829e2559a81fa02f4b
samim.azimi <samimelite8@gmail.com>
2022-01-18 13:49:05 +0430

    init

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './style/root.css'
+import React from 'react'
+
+
+
+
+
+function App() {
+  return (
+    <div className="landing_page_container">
+      <h1>Mudassir Printing Press</h1>
+
+      <ul>
+        <li>Home</li>
+        <li>Services</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+        <li>Customer Portal</li>
+      </ul>
+
+    </div>
+  );
+}
+
+export default App;
